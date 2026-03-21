a3fc7e332775fcde7e1f5de74cb0157602bd0d27
Prabhdeep Singh <26prabhdeep@gmail.com>
2026-03-21 17:53:41 +0530

    remove cors

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -4,7 +4,7 @@ date.setDate(date.getDate() - 1);
 
 const yesterday = date.toISOString().split("T")[0];
 
-export const newsApi_BaseURL = 'https://cors-anywhere.com/https://newsapi.org/v2/everything?q="';
+export const newsApi_BaseURL = 'https://newsapi.org/v2/everything?q="';
 
 // Use the 'yesterday' variable here
 export const newApi_filters = `"&from=${yesterday}&sortBy=publishedAt&apiKey=`;
