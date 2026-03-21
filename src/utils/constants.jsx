493cf634dd8f218d30f391f024cb469f3f0b4a62
Prabhdeep Singh <26prabhdeep@gmail.com>
2026-03-21 16:59:36 +0530

    updated api

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -1,3 +1,7 @@
+// Get today's date in YYYY-MM-DD format
+const today = new Date().toISOString().split("T")[0];
+
 export const newsApi_BaseURL = 'https://newsapi.org/v2/everything?q="';
 
-export const newApi_filters = '"&from=2026-03-19&sortBy=publishedAt&apiKey=';
+// We use a Template Literal here to inject the 'today' variable
+export const newApi_filters = `"&from=${today}&sortBy=publishedAt&apiKey=`;
