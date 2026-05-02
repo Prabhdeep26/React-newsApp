b3516008c8ee8340173c985313655c244861dc16
Prabhdeep Singh <26prabhdeep@gmail.com>
2026-05-02 16:56:48 +0530

    api call from backend

--- frontend/src/hooks/useSearchNews.jsx
+++ frontend/src/hooks/useSearchNews.jsx
@@ -2,18 +2,20 @@ import { useEffect, useState } from "react";
 
 const useFetchNews = (query) => {
 	const [result, setResult] = useState(null);
-	
+
 	const fetchNews = async () => {
-		const response = await fetch(`/api/search/${encodeURIComponent(query)}`, {
-			method: "GET",
-		});
+		const response = await fetch(
+			`https://react-news-hub-seven.vercel.app/api/search/${encodeURIComponent(query)}`,
+			{
+				method: "GET",
+			},
+		);
 		const json = await response.json();
 		setResult(json.articles);
-		
 	};
 	useEffect(() => {
 		fetchNews();
-	// eslint-disable-next-line react-hooks/exhaustive-deps
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [query]);
 
 	return result;
